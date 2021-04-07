a61d24d92422d0aadf87d5ecd6e15159107fafb4
Rachael Homann <rachaelhomann@gmail.com>
2021-04-07 13:06:45 -0500

    added honey script font for main landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,15 +30,15 @@ function Navbar() {
        <nav className="navbar">
         <div className="navbar-container">
             <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                MOXIE<i class="fa fa-star-o fa-xs" aria-hidden="true"></i>MASSAGE
+                MOXIE MASSAGE
             </Link>
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Home
+                    <Link to='/Bios' className='nav-links' onClick={closeMobileMenu}>
+                        Our Therapists
                     </Link>
                 </li>
                 <li className='nav-item'>
@@ -52,8 +52,8 @@ function Navbar() {
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
-                        Sign Up
+                    <Link to='/yoga' className='nav-links' onClick={closeMobileMenu}>
+                        Yoga
                     </Link>
                 </li>
             </ul>
