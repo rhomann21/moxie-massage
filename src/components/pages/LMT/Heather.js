5235268f62d89e1421c7df61cfd3794fc91d2414
Rachael Homann <rachaelhomann@gmail.com>
2021-04-15 12:40:41 -0500

    added Heather and Kayla bio page skeleton

---
+++ src/components/pages/LMT/Heather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Bios.css';
+import Heather from '../../../images/Heather.jpg';
+
+function Heather() {
+  return (
+    <div className='Heather'>
+
+    </div>
+  );
+}
+
+export default Heather;
---
+++ src/components/pages/LMT/Kayla.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Bios.css';
+import Kayla from '../../../images/Kayla.jpg';
+
+function Kayla() {
+  return (
+    <div className='Kayla'>
+
+    </div>
+  );
+}
+
+export default Kayla;
