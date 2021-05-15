94277ea93568e71be7d8c44e39c63b2d7ae14cb7
Rachael Homann <rachaelhomann@gmail.com>
2021-05-14 19:29:58 -0500

    contact page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/Signup';
@@ -11,7 +11,8 @@ import HeatherBio from './components/pages/LMT/HeatherBio';
 import KaylaBio from './components/pages/LMT/KaylaBio';
 import KendraBio from './components/pages/LMT/KendraBio';
 import Yoga from './components/pages/Yoga';
-import Services from './components/pages/Services/Services'
+import Services from './components/pages/Services/Services';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
@@ -21,13 +22,14 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/services' component={Services} />
-        <Route path='/sroducts' component={Products} />
+        <Route path='/products' component={Products} />
         <Route path='/signup' component={SignUp} />
         <Route path='/bios' component={Bios} />
         <Route path='/HeatherBio' component={HeatherBio} />
         <Route path='/KaylaBio' component={KaylaBio}   />
         <Route path='/KendraBio' component={KendraBio} />     
         <Route path='/yoga' component={Yoga} />
+        <Route path ='/contact' component={Contact} />
       </Switch>
       <FooterSection />
     </Router>
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../../App.css';
+
+
+//Include phone number for shop, email address, physical address
+//Do we need or want the google map feature?
+//Check out contact pages of other massage websites
+
+function Contact() {
+     return(
+    <>
+    </>
+     )}
+     
+export default Contact;
+
+//Make sure to link this page and other exsting pages into the footer 
