0c5bad79e675737e02da7f3bf6323bc65c06c6c7
Rachael Homann <rachaelhomann@gmail.com>
2021-06-18 09:41:40 -0500

    added background images to all 3 about pages, added some text blurbs, awaiting copy from owner.

--- src/components/pages/About/Massage101.js
+++ src/components/pages/About/Massage101.js
@@ -5,7 +5,13 @@ export default function Massage101() {
     return(
     <> <h1 className='massage101'>Massage 101</h1>
     <div className='massage101__container'>
-
+        <div className='massage101__text__wrap'>
+            <h3>How is massage beneficial to you?</h3>
+        <div className='massage101__p__wrap'>
+            <p>This is where we talk about massage and its benefits including relaxation, lowering blood pressure, pain relief, etc.</p>
+            <p>How can we help? What can we do for people looking for these services?</p>
+        </div>    
+        </div>
     </div>
     </>
 )}
--- src/components/pages/About/WhatWeDo.js
+++ src/components/pages/About/WhatWeDo.js
@@ -5,7 +5,19 @@ export default function WhatWeDo() {
     return(
     <> <h1 className='wwd'>What We Do</h1>
     <div className='wwd__container'>
+        <div className='wwd__text__wrapper'>
+            <h3>Moxie Massage welcomes you to experience relaxation at its finest.</h3>
+            <div className='wwd__text'>
+                <p> Moxie Massage began it’s journey 17 years ago with a dream, a hearty amount of determination, and unbelievable passion. Heather Fowler began her career ____________________. Since then, we have grown into a multi-faceted small business offering everything from adorable tote bags to beginner’s yoga classes.</p>
 
+                <p>Moxie Massage, voted best Massage therapists in Springfield, IL - welcomes you to experience what everyone is talking about, and see for yourself what it is like to feel good again.</p>
+
+                <p>A hidden gem, located downtown at 605 E. Washington Street, you are invited to stop in and enjoy the serene escape from your everyday stresses.</p>
+
+                <p>Whether you are in need of Swedish Massage, Deep Tissue, Hot Stone, Tui Na, Thai, Reflexology, or Body Treatments such as our slimming seaweed and chocolate wraps, we thank you for making us your favorite place to unwind.</p>
+
+            </div>
+        </div>
     </div>
     </>
 )}
