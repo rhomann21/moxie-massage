e49ebdaa0f484db046ff2d8a30d97ffece3a31ec
Rachael Homann <rachaelhomann@gmail.com>
2021-08-18 14:06:41 -0500

    ids, times, and prices added to dropdown in massagecarditem

--- src/components/pages/Services/MassageCardItem.js
+++ src/components/pages/Services/MassageCardItem.js
@@ -6,10 +6,17 @@ function MassageCardItem(props) {
     <>
       <li className='massages__item'>
         <div className='massages__item__card'>
-          <div className='massages__item__title-wrap'>{props.label}</div>
-          <div className='massage__time__info'>{props.time}</div>
+          <div className='massages__item__title__wrap'>{props.title}</div>
           <div className='massage__item__pricing'>{props.price}</div>
           <div className='massages__item__text'>{props.text}</div>
+          <div className='massage__time__info'>
+            <select className='time__dropdown'>
+                <option selected value='time'>Select a time</option>
+                <option value='time_30'>{props.time1}</option>
+                <option value='time_60'>{props.time2}</option>
+                <option value='time_90'>{props.time3}</option>
+            </select>
+          </div>
         </div>
       </li>
     </>
--- src/components/pages/Services/Massages.js
+++ src/components/pages/Services/Massages.js
@@ -5,13 +5,8 @@ import './MassageItems.css';
 import { Button } from '../../Button';
 
 
-// Add when possible:
-// data-service-id for each massage 
-// drop down for 30/60/90
-// individual id for each time for each massage
-// insert with string literal 
-// pop up book button with link to MB
-// data-service-id='${ID HERE}' /> assign to button
+
+//if time 1 is selected, show div with price 1, show book now button for that massage / id 1
 
 function Massages() {
     return (
@@ -30,47 +25,50 @@ function Massages() {
         <div className='massages__wrapper'>
           <ul className='massages__items'>
             <MassageCardItem
-            label='Deep Rest Massage'
-            time='30 / 60 / 90 minute'
-            price='$55 / $80 / $120'
+            title='Deep Rest Massage'
+            time1='30 minute'
+            time2='60 minute'
+            time3='90 minute'
+            price1='$55'
+            price2='$80'
+            price3='$120'
+            id1='100009'
+            id2='100008'
+            id3='1000026'
             text='This a deeply restorative massage with medium pressure and slow, purposeful strokes. This will calm your mind, relax your muscles and refresh your soul! It is truly a tiny vacation on the table.'
             />
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Deep Tissue Massage'
-            time='30 / 60 / 90 minute'
+            title='Deep Tissue Massage'
             price='$55 / $100 / $130'
             text='This is a DEEP massage. With a combination of elbows and feet, we use our whole body to strip your muscles down and use a heavy, even pressure to relieve tension and restrictions. We will not hurt you. We are in constant communication with you to ensure a therapeutic experience.'
             />
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Fix It! Massage'
-            time='30 / 60 / 90 minute'
+            title='Fix It! Massage'
             price='$55 / $100 / $130'
             text='Not sure what you need but you know it needs to be fixed? This appointment will cover everything! From recognizing and addressing the issue, the actual massage, and any self care you are taught following the massage. This is for when you need more than just a general massage and require some follow up.'
             />
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Pain Relief Massage'
-            time='30 / 60 / 90 minute'
+            title='Pain Relief Massage'
             price='$55 / $80 / $120'
             text='This is a combination of Swedish and deep tissue techniques to bring IMMEDIATE pain relief and reduce tension. It can be a relaxing massage to comfort tense muscles or it can be an intense trigger point session to break up adhesions in the tissue, depending on what you need.'
             />
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Swedish Circulation Massage'
-            time='30 / 60 / 90 minute'
+            title='Swedish Circulation Massage'
             price='$55 / $80 / $120'
             text='This massage is made up of deep kneading and really pushing the muscles out for a good stretch. If you sit at a desk all day, or drive a lot, or are just “stiff” this is the massage for you!'
             />
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Reflexology Massage'
+            title='Reflexology Massage'
             time='45 minute'
             price='$55'
             text='Reflexology uses certain points or "reflex areas" on the feet and hands to connect energetically to specific organs and body parts through energy channels in the body. We put pressure on these reflex areas to remove energy blockages and promote health in the related body areas.'
@@ -78,7 +76,7 @@ function Massages() {
             </ul>
             <ul className='massages__items'>
             <MassageCardItem
-            label='Hot Stone Massage'
+            title='Hot Stone Massage'
             time='60 / 90 minute'
             price='$100 / $140'
             text='Using basalt or pink Himalayan salt stones, penetrating heat from smooth stones is used to relieve pain and tension.'
@@ -86,7 +84,7 @@ function Massages() {
           </ul>
           <ul className='massages__items'>
             <MassageCardItem
-            label='Perfect Pregnancy Massage'
+            title='Perfect Pregnancy Massage'
             time='60 / 90 minute'
             price='$90 / $130'
               text='This massage is AMAZING. Relaxing Swedish strokes combined with some Thai massage on the hips. We work from ears to toes, and when you stand up you’ll wish you could stay forever! Safe, side-lying position. Seriously, after people deliver they come back requesting this massage.'
@@ -94,7 +92,7 @@ function Massages() {
             </ul>          
             <ul className='massages__items'>
             <MassageCardItem
-            label='Heather Heals Massage'
+            title='Heather Heals Massage'
             time='60 / 90 minute'
             price='$80 / $120'
               text='Two time winner of Best of Springfield, come see for yourself what the fuss is about. This is Heather’s signature style of 25 years of experience and all 140 modalities of massage she’s learned. With two years of medical school and 3 years of Chinese medicine under her belt, it makes for a unique and healing experience.'
@@ -102,7 +100,7 @@ function Massages() {
           </ul>
           <ul className='massages__items'>
             <MassageCardItem
-            label='Tui Na Massage'
+            title='Tui Na Massage'
             time='60 / 90 minute'
             price='$100 / $120'
               text='Heather has two years of training focusing on this special skill. The main benefit of Tui Na massage is that it focuses on the specific problem, whether it is an acute or a chronic pain associated with the joints, muscles, or a skeletal system. This technique is very beneficial in reducing neck, shoulder, hip, back, arm, thigh, leg, and ankle disorders. It is a very effective therapy for arthritis, pain, sciatica, and muscle spasms.'
@@ -110,7 +108,7 @@ function Massages() {
           </ul>
           <ul className='massages__items'>
             <MassageCardItem
-            label='Couples Massage'
+            title='Couples Massage'
             time='60 minute'
             price='$160'
               text='Let us know what you and your plus one need! Swedish, pain relief, deep tissue, deep rest, and fix-it are available in this package.'
@@ -118,7 +116,7 @@ function Massages() {
           </ul>
           <ul className='massages__items'>
             <MassageCardItem
-            label='TMJ Massage for Deep Healing'
+            title='TMJ Massage for Deep Healing'
             time='30 minute'
             price='$55'
               text='This massage uses specific protocol for temporomandibular joint disfunction - helps to relieve migraines and teeth grinding.'
@@ -127,7 +125,7 @@ function Massages() {
         </div>
         <ul className='massages__items'>
             <MassageCardItem
-            label='Myofascial Release Massage'
+            title='Myofascial Release Massage'
             time='60 minute'
             price='$90'
               text='Myofascial release is a gentle form of holistic body work using the osteopathic work and teachings of Ida Rolf. It incorporates working with the client and their nervous system, breathing deeply, and using very little lotion or oil. We work deeper, layer by layer, in order to target stuck tissue. Great for neck and shoulder pain, sciatica, and plantar fasciitis.'
