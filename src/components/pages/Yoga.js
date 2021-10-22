f1f80ef18da2cac68d5589fb80573e2b1248e618
Rachael Homann <rachaelhomann@gmail.com>
2021-10-22 14:08:23 -0500

    facebook plugin

--- src/components/pages/Yoga.js
+++ src/components/pages/Yoga.js
@@ -1,43 +1,58 @@
 import React from 'react';
 import { useEffect } from 'react';
-import MembershipCardItem from './Membership/MembershipCardItem';
 import '../../App.css';
 
 
+{/* <script async defer crossorigin="anonymous" 
+src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="f12vbQmt">
+</script>
+ */}
+
 function Yoga() {
+  // <div id="fb-root"></div>
 
-
+  
     useEffect(() => {
       window.scrollTo(0, 0)
-    }, [])
-  
-    return(
-    <><div><h1 className='yoga'>Yoga</h1></div>
+    }, []);
+
+
+    // useEffect(() => {
+    //       FB.XFBML.parse();
+    //         });
+
+    return(    
+    <>
+
+{/* window.fbAsyncInit = () => {
+        FB.init({
+            appId: '82195',
+            xfbml: false,
+            version: 'v2.6'
+        });
+        document.dispatchEvent(new Event('fb_init'));
+    }; */}
+
+    
+    <div><h1 className='yoga'>Yoga</h1></div>
    
-   <div className='membership__cards'>
-      <div className='membership__intro__text'>
+   <div className='yogaWrapper'>
+      <div className='yogaTitleText'>
      <p> Now introducing Moxie Yoga! Beginner's classes are now available online or in studio at our Spa. No judgement, no pressure - just a good stretch and a great time.</p>
       </div>
-      <div className='membership__cards__container'>
-        <div className='membership__cards__wrapper'>
-          <ul className='membership__cards__items'>
-            <MembershipCardItem
-               title='In Studio'
-               label='$15 per session'
-              text='Option 1: 1 hour Swedish massage + 1 yoga class.'
-              text2='Option 2: 1 hour deep tissue massage  + 1 yoga class.'
-            //   id is from mindbosy in booking url link (last three numbers)
-              id='100116'
-            />           
-       <MembershipCardItem         
-               title='Online Option'
-               label='$10 per session'
-              text='Option 1: 1 hour Deep Rest massage + 1 yoga class + 10% off product.'
-              text2='Option 2: 1 hour Tui Na (Fix It) massage + 1 yoga class + 10% off product.'
-              id='102'
-            /> 
-         </ul>
-        </div>
+      <div className='yogaFacebookContainer'>
+      {/* <div class="fb-page" 
+        data-href="https://www.facebook.com/MoxieBacktini/" 
+        data-tabs="timeline" 
+        data-width="" 
+        data-height="" 
+        data-small-header="true" 
+        data-adapt-container-width="true" 
+        data-hide-cover="false" 
+        data-show-facepile="false">
+          <blockquote cite="https://www.facebook.com/MoxieBacktini/" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/MoxieBacktini/">Moxie Massage</a></blockquote>
+          </div> */}
       </div>
 
  </div>
