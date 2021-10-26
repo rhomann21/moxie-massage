58250a31c9ce61c9bf71122ba6aa7e366431fa99
Rachael Homann <rachaelhomann@gmail.com>
2021-10-26 11:08:20 -0500

    Bios now one page, added Kendra and Margie, still need updated bio picture

--- src/components/pages/LMT/KendraBio.js
+++ src/components/pages/LMT/KendraBio.js
@@ -9,25 +9,36 @@ function KendraBio() {
   }, [])
 
   return (
-    <div className='Kendra__wrapper'>
-        <div className='Kendra__bio__wrapper'>
-            <h1>Kendra Waide</h1>
-            <div className='Kendra__pic__container'>
-              <img className='Kendra__pic' src={Kendra} alt='kendra'></img>
-            </div>
-            <div className='Kendra__bio__container'>
-            <p>
-            Kendra became licensed in 2019 after graduating from Lincoln Land
-Community College - Capital City Training Center and becoming
-Certified in Myofascial Release by Holly Pinto of The Body Therapy
-Center and School of Massage in Swansea IL. Kendra has practiced the
-Japanese healing technique known as Reiki since 2015 and has earned
-the title of Reiki Master. She really loves to connect and help people
-and is excited to be part of Moxie Massage!
-</p>
+    <>
+    <div className='Heather__wrapper'>
+      <div className='HeatherTitleWrap'>
+          <div className='HeatherOwnerWrap'>
+                <h1> lmt</h1>
+          </div>
+          <div className='HeatherNameWrap'>
+            <h3>Kendra Waide,</h3>
+          </div>
+      </div>
+      
+        <div className='Heather__bio__wrapper'>
+        <div className="Heather__pic__container">
+              <img className='Heather__pic' src={Kendra} alt='Kendra'></img>
+        </div>
+        
+            <div className='Heather__bio__container'>
+              <p>Kendra became licensed in 2019 after graduating from Lincoln Land
+              Community College - Capital City Training Center and becoming
+              Certified in Myofascial Release by Holly Pinto of The Body Therapy
+              Center and School of Massage in Swansea IL. </p>
+              <p> Kendra has practiced the Japanese healing technique known as Reiki since 2015 and has earned
+              the title of Reiki Master. She really loves to connect and help people
+              and is excited to be part of Moxie Massage!</p>
+              <br />
+              <p>217.652.8157</p>
             </div>
         </div>
     </div>
+    </>
   );
 }
 
