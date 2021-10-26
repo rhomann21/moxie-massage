3d5544aa00e7dbfd8918eca9b3c14dab45d216ee
Rachael Homann <rachaelhomann@gmail.com>
2021-10-26 11:10:32 -0500

    working on fb plugin

--- src/components/pages/Yoga.js
+++ src/components/pages/Yoga.js
@@ -3,36 +3,42 @@ import { useEffect } from 'react';
 import '../../App.css';
 
 
+
 {/* <script async defer crossorigin="anonymous" 
 src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="f12vbQmt">
-</script>
- */}
+</script> */}
+
+
+
+// ((d, s, id) => {
+// 	var js, fjs = d.getElementsByTagName(s)[0];
+// 	if (d.getElementById(id)) {return;}
+// 	js = d.createElement(s); js.id = id;
+// 	js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0";
+// 	fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+// let window.fbAsyncInit = () => {
+// 	//SDK loaded, initialize it
+// 	FB.init({
+// 		appId      : 'your-app-id',
+// 		xfbml      : true,
+// 		version    : 'v2.2'
+// 	});
+
+
 
 function Yoga() {
-  // <div id="fb-root"></div>
-
   
     useEffect(() => {
       window.scrollTo(0, 0)
     }, []);
 
 
-    // useEffect(() => {
-    //       FB.XFBML.parse();
-    //         });
-
     return(    
     <>
 
-{/* window.fbAsyncInit = () => {
-        FB.init({
-            appId: '82195',
-            xfbml: false,
-            version: 'v2.6'
-        });
-        document.dispatchEvent(new Event('fb_init'));
-    }; */}
-
+  <div id="fb-root"></div>
     
     <div><h1 className='yoga'>Yoga</h1></div>
    
@@ -41,7 +47,7 @@ function Yoga() {
      <p> Now introducing Moxie Yoga! Beginner's classes are now available online or in studio at our Spa. No judgement, no pressure - just a good stretch and a great time.</p>
       </div>
       <div className='yogaFacebookContainer'>
-      {/* <div class="fb-page" 
+       <div class="fb-page" 
         data-href="https://www.facebook.com/MoxieBacktini/" 
         data-tabs="timeline" 
         data-width="" 
@@ -52,7 +58,7 @@ function Yoga() {
         data-show-facepile="false">
           <blockquote cite="https://www.facebook.com/MoxieBacktini/" class="fb-xfbml-parse-ignore">
             <a href="https://www.facebook.com/MoxieBacktini/">Moxie Massage</a></blockquote>
-          </div> */}
+          </div>
       </div>
 
  </div>
